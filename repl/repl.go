abc7d498a73dcc3fca79d7af5ae4768592a27ff7
Xing, Wang <zenja.wang@gmail.com>
2016-12-20 21:33:14 +0800

    repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/zenja/monkey-lang/evaluator"
 	"github.com/zenja/monkey-lang/lexer"
 	"github.com/zenja/monkey-lang/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
