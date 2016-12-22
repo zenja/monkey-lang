d400b4218feaa72ef6d6e299bf5702f951e9f638
Xing, Wang <xwang16@ebay.com>
2016-12-22 16:54:02 +0800

    early stop in eval when has error

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,6 +26,9 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.ReturnStatement:
 		v := Eval(node.ReturnValue)
+		if isError(v) {
+			return v
+		}
 		return &object.ReturnValue{Value: v}
 
 	// Expressions
@@ -37,11 +40,20 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
+		if isError(right) {
+			return right
+		}
 		return evalPrefixExpression(node.Operator, right)
 
 	case *ast.InfixExpression:
 		left := Eval(node.Left)
+		if isError(left) {
+			return left
+		}
 		right := Eval(node.Right)
+		if isError(right) {
+			return right
+		}
 		return evalInfixExpression(node.Operator, left, right)
 
 	case *ast.IfExpression:
@@ -177,6 +189,9 @@ func evalIntegerInfixExpression(
 
 func evalIfExpression(ie *ast.IfExpression) object.Object {
 	condition := Eval(ie.Condition)
+	if isError(condition) {
+		return condition
+	}
 	if isTruthy(condition) {
 		return Eval(ie.Consequence)
 	} else if ie.Alternative != nil {
@@ -201,4 +216,11 @@ func isTruthy(obj object.Object) bool {
 
 func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
+}
+
+func isError(obj object.Object) bool {
+	if obj == nil {
+		return false
+	}
+	return obj.Type() == object.ERROR_OBJ
 }
